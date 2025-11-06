8d704890d04a73f03dffd8b12ab05ef2a3b86fc0
Martijn Bogaert <martijn.bogaert@sirus.be>
2025-11-06 10:10:20 +0100

    Explain delta dispatch to filtered graph

--- config/consumers/decisions-ghent/delta-sync-dispatching.js
+++ config/consumers/decisions-ghent/delta-sync-dispatching.js
@@ -16,5 +16,29 @@ export async function dispatch(lib, data) {
 
     await deleteFromGraph(deletes, sparqlEndpoint, INGEST_GRAPH, {});
     await insertIntoGraph(inserts, sparqlEndpoint, INGEST_GRAPH, {});
+
+    // -- FILTERED GRAPH --
+
+    // DELETE: just delete from filtered graph
+
+    // INSERT:
+
+    // Necessities:
+    // - list of unique properties
+    // - list of unique types (incl. rdf:type and mu:uuid)
+    // - property path to bestuursorgaan for each type
+
+    // Steps:
+    // 1) check whether insert property is in list of properties
+    // 2) query subject's type from ingest graph
+    // 3) check whether suject's type is in list of types
+    // 4) query ingest graph whether subject belongs to bestuursorgaan (use property path)
+    // 5) if all checks succeed, insert into filtered graph
+
+    // Create task:
+    // - new task:Task
+    // - task:operation: <http://lblod.data.gift/id/jobs/concept/TaskOperation/mapping/sparql-construct>
+    // - adms:status: <http://redpencil.data.gift/id/concept/JobStatus/scheduled>
+    // - task:inputContainer: with ext:hasResource pointing to insert's subject
   }
 }
