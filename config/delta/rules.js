ea959a0b8f9792c340932dc72b753c6ecf0f0b17
Martijn Bogaert <martijn.bogaert@sirus.be>
2026-02-02 21:07:55 +0100

    Add delta rules

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,5 +1,6 @@
 import harvest from "./harvest";
 import jobController from "./job-controller";
 import pdfToEli from "./pdf-to-eli";
+import osloToEli from "./oslo-to-eli";
 
-export default [...harvest, ...jobController, ...pdfToEli];
+export default [...harvest, ...jobController, ...pdfToEli, ...osloToEli];
