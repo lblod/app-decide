7e83195af3554dc4b25a07d1bb92faa76d7b5b28
Martijn Bogaert <36764916+MartijnBogaert@users.noreply.github.com>
2026-02-27 09:08:04 +0100

    Automate OSLO-ELI pipeline (#62)
    * Add pipeline services to docker compose
    
    * Add delta rules
    
    * Add job controller config
    
    * Add operation uris
    
    * Revert landing graph uri
    
    * Temporarily remove first operation
    
    * Lower transformation batch size
    
    * Use high load data base endpoint
    
    * Get rid of high load database endpoints
    
    * Add migrations for tijdpecialisaties
    
    * Only keep necessary properties of tijdspecialisaties
    
    * Merge branch 'development' into feature/automate-oslo-eli-pipeline
    
    * Merge branch 'feature/add-tijdspecialisaties' into feature/automate-oslo-eli-pipeline
    
    * Merge branch 'development' into feature/automate-oslo-eli-pipeline
    
    * Add org graph env var
    
    * Update env vars
    
    * Extend oslo eli pipeline
    
    * Merge branch 'development' into feature/automate-oslo-eli-pipeline
    
    * Add missing singleton job
    
    * Merge branch 'development' into feature/automate-oslo-eli-pipeline
    
    * Bump oslo-eli filter and transformer services
    
    * Enable oslo pipeline in harvester frontend
    
    * Bump lokaal beslist consumer service

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,6 +1,13 @@
-import harvest from './harvest';
-import jobController from './job-controller';
-import pdfToEli from './pdf-to-eli';
-import search from './search';
+import harvest from "./harvest";
+import jobController from "./job-controller";
+import pdfToEli from "./pdf-to-eli";
+import osloToEli from "./oslo-to-eli";
+import search from "./search";
 
-export default [...harvest, ...jobController, ...pdfToEli, ...search];
+export default [
+  ...harvest,
+  ...jobController,
+  ...pdfToEli,
+  ...osloToEli,
+  ...search,
+];
