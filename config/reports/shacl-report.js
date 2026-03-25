1c9d7a962767251a3488252cd5789f7e499b2fc8
brechtvdv <br5cht@hotmail.com>
2026-03-25 15:32:56 +0100

    Validation in batches

--- config/reports/shacl-report.js
+++ config/reports/shacl-report.js
@@ -21,14 +21,16 @@ export const BATCH_SIZE = env
   .default('100')
   .asIntPositive();
 
-import { sparqlEscapeUri } from "mu";
+import { sparqlEscapeUri, uuid } from "mu";
 import { querySudo } from '@lblod/mu-auth-sudo';
 
 import { Store, DataFactory } from "n3";
-const { namedNode } = DataFactory;
+import { report } from "node:process";
+const { namedNode, literal } = DataFactory;
 
 const CRON_PATTERN = "0 3 * * *";
 const REPORT_NAME = "ELI validation of Decide";
+const MAX_STRING_LENGTH = '100'; // string values will be shortened when inserting in temporary validation graph for sparql-based constraints
 
 const namedGraphs = [
     'http://mu.semte.ch/graphs/public/freiburg',
@@ -51,29 +53,22 @@ const cronFunction = async (namedGraph = null) => {
             sparqlShapeDataset
         );
         
-        const dataDataset = new Store();
-
-        fillDataDataset(dataDataset, shapesDataset, sparqlShapeDataset);
-
-        console.log(
-            `Running SHACL validation on store of size ${dataDataset.size}...`
-        );
-        const startTime = Date.now();
-        const reportDataset = await validateDataset(dataDataset, shapesDataset);
-        console.log(`Running SPARQL-based constraints...`);
-        await addSparqlValidationsToReport(
-            dataDataset,
-            reportDataset,
-            sparqlValidationObjects
-        );
-        const endTime = Date.now();
-        console.log(
-            `SHACL validation took ${(endTime - startTime) / 1000} seconds.`
-        );
-
-        await saveDatasetToNamedGraphs(reportDataset, namedGraphs);
-        console.log(`SHACL validation report saved in triple store.`);
-
+        const allTargetClasses = retrieveTargetClasses([shapesDataset, sparqlShapeDataset]);
+        // Validation happens in batches (fill, validate, save), so we don't need to insert all data in memory first
+        // Key is to pass a reportURI to the validation function, so all results are linked to the same report
+        const reportUUID = uuid();
+        const reportURI = `http://data.lblod.info/id/reports/${reportUUID}`;
+        for (const targetClass of allTargetClasses) {
+            const count = await countResources(targetClass, namedGraphs);
+            console.log(`Adding ${count} resources for graphs ${safeNamedGraphs} and resource type ${targetClass}...`);
+            for (let offset = 0; offset < count; offset += BATCH_SIZE) {
+                const dataDataset = new Store();
+                await fillDataDataset(targetClass, offset, dataDataset, shapesDataset, sparqlShapeDataset);
+                const batchReportDataset = await validateShapesAndSparql(dataDataset, shapesDataset, sparqlValidationObjects, reportURI);
+                await saveDatasetToNamedGraphs(batchReportDataset, namedGraphs);
+            }
+            console.log(`SHACL validation done for target class ${targetClass}.`);
+        }
         if (ONLY_KEEP_LATEST_REPORT) {
             await deletePreviousShaclValidationReports(namedGraphs);
         }
@@ -94,47 +89,68 @@ export default {
   execute: cronFunction,
 };
 
-// Fill dataDataset, one level deep (?resource ?p ?o), with resources of type target class in the shape files and sparql-based shapes
-async function fillDataDataset(dataDataset, shapesDataset, sparqlShapeDataset) {
-    const targetClassesShacl = [...shapesDataset.match(
-        null,
-        namedNode("http://www.w3.org/ns/shacl#targetClass"),
-        null
-    )].map(q => q.object.value);
-    const targetClassesSparql = [...sparqlShapeDataset.match(
-        null,
-        namedNode("http://www.w3.org/ns/shacl#targetClass"),
-        null
-    )].map(q => q.object.value);
-    const allTargetClasses = [
-        ...new Set([
-            ...targetClassesShacl,
-            ...targetClassesSparql
-        ])
-    ];
-    for (const targetClass of allTargetClasses) {
-        const count = await countResources(targetClass, namedGraphs);
-        console.log(`Adding ${count} resources for graphs ${safeNamedGraphs} and resource type ${targetClass}...`);
-        for (let offset = 0; offset < count; offset += BATCH_SIZE) {
-            const resources = [];
-            const resourcesResult = await querySudo(`
-            SELECT DISTINCT ?resource
-            WHERE {
-                    VALUES ?graph {
-                        ${safeNamedGraphs}
-                    }
+// Fill dataDataset, one level deep (?resource ?p ?o), with resources of type target class
+async function fillDataDataset(targetClass, offset, dataDataset) {
+    const resources = [];
+    const resourcesResult = await querySudo(`
+    SELECT DISTINCT ?resource
+    WHERE {
+            VALUES ?graph {
+                ${safeNamedGraphs}
+            }
 
-                    GRAPH ?graph {
-                        ?resource a ${sparqlEscapeUri(targetClass)} .
-                    }
-                }
-            LIMIT ${BATCH_SIZE}
-            OFFSET ${offset}
-            `);
-            resourcesResult.results.bindings.forEach((binding) => {
-                resources.push(binding.resource.value);
-            });
-            await addResourcesOneLevelDeep(dataDataset, namedGraphs, resources);
+            GRAPH ?graph {
+                ?resource a ${sparqlEscapeUri(targetClass)} .
+            }
         }
-    }
+    LIMIT ${BATCH_SIZE}
+    OFFSET ${offset}
+    `);
+    resourcesResult.results.bindings.forEach((binding) => {
+        resources.push(binding.resource.value);
+    });
+    await addResourcesOneLevelDeep(dataDataset, namedGraphs, resources);
+
+    // Shorten strings to avoid insert problems
+    dataDataset.forEach((quad) => {
+        if (quad.object.termType === 'Literal' && typeof quad.object.value === 'string') {
+            const shorterObject = literal(quad.object.value.substr(0, MAX_STRING_LENGTH), quad.object.datatype)
+            dataDataset.addQuad(quad.subject, quad.predicate, shorterObject);
+            dataDataset.removeQuad(quad);
+        }
+    })
+}
+
+async function validateShapesAndSparql(dataDataset, shapesDataset, sparqlValidationObjects, reportURI) {
+    console.log(
+            `Running SHACL validation on store of size ${dataDataset.size}...`
+    );
+    const startTime = Date.now();
+    console.log(`Running non-SPARQL-based constraints...`);
+    const reportDataset = await validateDataset(dataDataset, shapesDataset, reportURI);
+    console.log(`Running SPARQL-based constraints...`);
+    await addSparqlValidationsToReport(
+        dataDataset,
+        reportDataset,
+        sparqlValidationObjects
+    );
+    const endTime = Date.now();
+    console.log(
+        `SHACL validation took ${(endTime - startTime) / 1000} seconds.`
+    );
+    return reportDataset;
+}
+
+function retrieveTargetClasses(datasets) {
+    const allTargetClasses = new Set();
+    datasets.forEach(dataset => {
+        [...dataset.match(
+            null,
+            namedNode("http://www.w3.org/ns/shacl#targetClass"),
+            null
+        )]
+        .map(q => allTargetClasses.add(q.object.value));
+    });
+    console.log("target classes found: " + allTargetClasses.toString());
+    return allTargetClasses;
 }
