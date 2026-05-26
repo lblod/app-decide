ffe794e50254a90362ae45d1135d78c02280968c
karel kremer <karel.kremer@redpencil.io>
2026-05-26 14:23:08 +0200

    remove delta rules that no longer matter

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,19 +1,3 @@
-import harvest from './harvest';
-import jobController from './job-controller';
-import pdfToEli from './pdf-to-eli';
-import osloToEli from './oslo-to-eli';
-import search from './search';
 import resource from './resource';
-import codelist from './codelist';
-import annotationJobSplitter from './annotation-job-splitter';
 
-export default [
-  ...resource,
-  ...harvest,
-  ...jobController,
-  ...pdfToEli,
-  ...osloToEli,
-  ...search,
-  ...codelist,
-  ...annotationJobSplitter,
-];
+export default [...resource];
