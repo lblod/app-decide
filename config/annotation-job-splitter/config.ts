4d0979a0f7f22048963f7601ab38f5a0497106b2
onodrim <alexander.van.den.berghe@redpencil.io>
2026-08-03 10:08:41 +0200

    fix(pdf-to-eli): remove split for pdf-scraping tasks
    Having this split task would require that the harvesting dashboard and
    pdf-content service contain logic to determine when to create/update the target
    shape for a job with PDF harvesting.
    
    More specifically, the dashboard would have to create shape for pdf-to-eli jobs
    but not for pdf-to-enriched jobs. Inversely, the pdf-content service would have
    to insert as a shape for pdf-to-enriched jobs, but not for pdf-to-eli.
    
    To avoid this additional complexity we opted to avoid the splitting of scraping
    tasks altogether.  The disadvantage is that restarting such tasks will require
    redoing more work.

--- config/annotation-job-splitter/config.ts
+++ config/annotation-job-splitter/config.ts
@@ -54,18 +54,6 @@ export default {
           },
         ],
       },
-    'http://lblod.data.gift/id/jobs/concept/JobOperation/harvesting/pdf-to-eli':
-      {
-        taskConfiguration: [
-          {
-            currentOperation:
-              'http://lblod.data.gift/id/jobs/concept/TaskOperation/split-task-pdf-to-eli-scraping',
-            nextOperation:
-              'http://lblod.data.gift/id/jobs/concept/TaskOperation/pdf-scraping',
-            harvestingCollection: true,
-          },
-        ],
-      },
     'http://lblod.data.gift/id/jobs/concept/JobOperation/harvesting/pdf-to-enriched':
       {
         taskConfiguration: [
