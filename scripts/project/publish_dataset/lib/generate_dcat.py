0b1d1dc37af5c01e7614fba9f5e757e62c5a9617
karel kremer <karel.kremer@redpencil.io>
2026-08-28 08:35:25 +0200

    extract type constants, fix delete_subjects getting too few arguments

--- scripts/project/publish_dataset/lib/generate_dcat.py
+++ scripts/project/publish_dataset/lib/generate_dcat.py
@@ -5,15 +5,20 @@ env = Environment(loader=FileSystemLoader("."))
 from helpers import graph_has_type, delete_subjects, turtle_to_insert_data, update, log, delete_linked_resources, delete_reverse_linked_resources
 from config import datadump_file_name, dataset_uri_and_uuid, distribution_uri_and_uuid, service_uri_and_uuid, PUBLIC_GRAPH
 
+DCAT_CATALOG = "http://www.w3.org/ns/dcat#Catalog"
+DCAT_DATASET = "http://www.w3.org/ns/dcat#Dataset"
+DCAT_DISTRIBUTION = "http://www.w3.org/ns/dcat#Distribution"
+DCAT_DATASERVICE = "http://www.w3.org/ns/dcat#DataService"
+
 def step1_write_catalog(organization: str, organization_config: dict, now_iso: str) -> None:
     log("[Step 1] Generate DCAT Catalog for %s", organization)
     catalog_uri = organization_config["catalog_uri"]
     catalog_publisher_uri = organization_config["catalog_publisher"].get("uri")
     
-    if graph_has_type(catalog_uri, "http://www.w3.org/ns/dcat#Catalog", PUBLIC_GRAPH):
+    if graph_has_type(catalog_uri, DCAT_CATALOG, PUBLIC_GRAPH):
         log("Catalog '%s' already exists in <%s>, doing nothing.", organization, PUBLIC_GRAPH)
     else:
-        delete_subjects([catalog_uri]) # in case there is a tombstone for it
+        delete_subjects([catalog_uri], PUBLIC_GRAPH) # in case there is a tombstone for it or any other information that claims it is some other type than a catalog
         catalog_template = env.get_template("templates/dcat-catalog.ttl.j2")
         catalog_output = catalog_template.render(
             ISSUED=now_iso,
@@ -32,12 +37,12 @@ def step2_write_dataset(organization: str, organization_config: dict, dataset: s
     insert_dataset = True
     insert_dataservice = True
     insert_distribution = True
-    if graph_has_type(dataset_uri, "http://www.w3.org/ns/dcat#Dataset", PUBLIC_GRAPH):
+    if graph_has_type(dataset_uri, DCAT_DATASET, PUBLIC_GRAPH):
         log("DCAT Dataset '%s' already exists in <%s>, only appending link to the service and distribution if they don't exist yet.", dataset, PUBLIC_GRAPH)
         insert_dataset = False
-        if graph_has_type(service_uri, "http://www.w3.org/ns/dcat#DataService", PUBLIC_GRAPH):
+        if graph_has_type(service_uri, DCAT_DATASERVICE, PUBLIC_GRAPH):
             insert_dataservice = False
-        if graph_has_type(distribution_uri, "http://www.w3.org/ns/dcat#Distribution", PUBLIC_GRAPH):
+        if graph_has_type(distribution_uri, DCAT_DISTRIBUTION, PUBLIC_GRAPH):
             insert_distribution = False
     
     values_to_delete = []
